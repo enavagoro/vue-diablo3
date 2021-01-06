dcdd3d57dc1af729fa145f47694e4ace8b49d03c
enavagoro <e.navagoro@gmail.com>
2021-01-05 23:46:13 -0300

    <feat>:<structure of project and calling outh token>

--- src/main.js
+++ src/main.js
@@ -1,12 +1,31 @@
-import Vue from 'vue'
+// archivos locales
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// npm
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+// CSS global
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  methods: {
+    // Nuestra función
+    init () {
+      store.dispatch('oauth/getToken', null, { root: true })
+    }
+  },
+  // Hook created
+  created () {
+    this.init()
+  },
   render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Home/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="hello">
+    <b-button>Just a Button!</b-button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+</script>
