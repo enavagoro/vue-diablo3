768c0bf5a65a563a34043f51d7d52d6805bceb0c
enavagoro <e.navagoro@gmail.com>
2021-01-07 23:57:36 -0300

    <feat>:<new home page and componentes>

---
+++ src/views/Home/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <HomeTitle/>
+  </div>
+</template>
+
+<script>
+import HomeTitle from './HomeTitle'
+
+export default {
+  name: 'Home',
+  components: {
+    HomeTitle
+  }
+}
+</script>
